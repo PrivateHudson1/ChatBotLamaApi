f4978f3a24c90bbda09fc69351c27a9f64f90c93
PvtHudson <rasheb782@mail.ru>
2025-09-21 20:22:51 +0500

    Create project, initialize SignalR

---
+++ Services/ChatHub.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace ChatBotLamaApi.Services
+{
+    public class ChatHub : Hub
+    {
+        public async Task SendMessage(string user, string message)
+        {
+            await this.Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
+
+    }
+}
