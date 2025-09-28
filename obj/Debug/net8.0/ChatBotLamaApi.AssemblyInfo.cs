12a8c2928ce874054a7de70174c833bc0d800edd
PvtHudson <rasheb782@mail.ru>
2025-09-28 21:19:38 +0500

    Add ApiKey Authentication Handler

--- obj/Debug/net8.0/ChatBotLamaApi.AssemblyInfo.cs
+++ obj/Debug/net8.0/ChatBotLamaApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ChatBotLamaApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f4978f3a24c90bbda09fc69351c27a9f64f90c93")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2c4c7a190948004bb5d2bd90ee63c67005e22529")]
 [assembly: System.Reflection.AssemblyProductAttribute("ChatBotLamaApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ChatBotLamaApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
