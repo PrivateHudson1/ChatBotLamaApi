2c94100de6a55017804132e1c113b6611bb73a23
PvtHudson <rasheb782@mail.ru>
2025-10-05 16:42:27 +0500

    Add .gitignore file

--- obj/Debug/net8.0/ChatBotLamaApi.AssemblyInfo.cs
+++ obj/Debug/net8.0/ChatBotLamaApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ChatBotLamaApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f5739ba9fa6047f3a8e02f5de1a63b7185476ed9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ec854b2bc735e231737e015ed5ef8ab68bffa13a")]
 [assembly: System.Reflection.AssemblyProductAttribute("ChatBotLamaApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ChatBotLamaApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
