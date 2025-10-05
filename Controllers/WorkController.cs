ea0904b4942a5c0acd64a5329eea5ddd76f5c13a
PvtHudson <rasheb782@mail.ru>
2025-10-05 16:47:00 +0500

    Clear Git cache

---
+++ Controllers/WorkController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ChatBotLamaApi.Controllers
+{
+    public class WorkController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
